720380a30160d597dfc279ec787f18cd5802dd8c
Richard J <techpowerawaits@outlook.com>
2021-03-27 20:39:33 -0600

    Remove uppercase variable names
    Fixes #12

--- get-latest.sh
+++ get-latest.sh
@@ -1,115 +1,115 @@
 #!/bin/bash
 
 # The mirror of Tomcat to download from
-MIRRORBASE="http://mirrors.ukfast.co.uk/sites/ftp.apache.org/tomcat"
+mirrorbase="http://mirrors.ukfast.co.uk/sites/ftp.apache.org/tomcat"
 
 # The major and minor versions of the version of Tomcat to download
-TOMCAT_MAJOR_VERSION=8
-TOMCAT_MINOR_VERSION=5
+tomcat_major_version=8
+tomcat_minor_version=5
 
 ###############################################################################
 
 # Helper variables
-COLOR_BOLD="\e[1m"
-COLOR_SUCCESS="\e[1;92m"
-COLOR_FAILURE="\e[1;91m"
-COLOR_RESET="\e[0m"
+color_bold="\e[1m"
+color_success="\e[1;92m"
+color_failure="\e[1;91m"
+color_reset="\e[0m"
 
 # Check configuration parameters
-if [ "x$MIRRORBASE" == "x" ]; then
-	echo -e "${COLOR_FAILURE}MIRRORBASE not set - edit script configuration before running${COLOR_RESET}"
+if [ "x$mirrorbase" == "x" ]; then
+	echo -e "${color_failure}mirrorbase not set - edit script configuration before running${color_reset}"
 	exit 1;
 fi
-if [ "x$TOMCAT_MAJOR_VERSION" == "x" ]; then
-	echo -e "${COLOR_FAILURE}TOMCAT_MAJOR_VERSION not set - edit script configuration before running${COLOR_RESET}"
+if [ "x$tomcat_major_version" == "x" ]; then
+	echo -e "${color_failure}tomcat_major_version not set - edit script configuration before running${color_reset}"
 	exit 1;
 fi
-if [ "x$TOMCAT_MINOR_VERSION" == "x" ]; then
-	echo -e "${COLOR_FAILURE}TOMCAT_MINOR_VERSION not set - edit script configuration before running${COLOR_RESET}"
+if [ "x$tomcat_minor_version" == "x" ]; then
+	echo -e "${color_failure}tomcat_minor_version not set - edit script configuration before running${color_reset}"
 	exit 1;
 fi
 
 # Download directory listing
-wget --quiet -O /tmp/tc${TOMCAT_MAJOR_VERSION}-index.html "${MIRRORBASE}/tomcat-${TOMCAT_MAJOR_VERSION}" > /dev/null
+wget --quiet -O /tmp/tc${tomcat_major_version}-index.html "${mirrorbase}/tomcat-${tomcat_major_version}" > /dev/null
 if [ "x$?" != "x0" ]; then
-	echo -e "${COLOR_FAILURE}Tomcat version check download failed${COLOR_RESET}"
+	echo -e "${color_failure}Tomcat version check download failed${color_reset}"
 	exit 1;
 fi
 
 # Get a list of version numbers that are available
-cat /tmp/tc${TOMCAT_MAJOR_VERSION}-index.html | fgrep '[DIR]' | grep -E "v${TOMCAT_MAJOR_VERSION}.${TOMCAT_MINOR_VERSION}.[0-9]+/" | grep -Eo '<a[ \t]+href[ \t]*=[ \t]*"[^"]*"' | grep -Eo "v${TOMCAT_MAJOR_VERSION}\.${TOMCAT_MINOR_VERSION}\.[^\"]*" | sed 's|/$||' > /tmp/tc${TOMCAT_MAJOR_VERSION}-versions.txt
+cat /tmp/tc${tomcat_major_version}-index.html | fgrep '[DIR]' | grep -E "v${tomcat_major_version}.${tomcat_minor_version}.[0-9]+/" | grep -Eo '<a[ \t]+href[ \t]*=[ \t]*"[^"]*"' | grep -Eo "v${tomcat_major_version}\.${tomcat_minor_version}\.[^\"]*" | sed 's|/$||' > /tmp/tc${tomcat_major_version}-versions.txt
 
 # Sort the release numbers to get the latest release
-TOMCAT_RELEASE=`cat /tmp/tc${TOMCAT_MAJOR_VERSION}-versions.txt | cut -d. -f3 | sort -nr | head -n1`
-TOMCAT_VERSION=${TOMCAT_MAJOR_VERSION}.${TOMCAT_MINOR_VERSION}.${TOMCAT_RELEASE}
-TOMCAT_URL=${MIRRORBASE}/tomcat-${TOMCAT_MAJOR_VERSION}/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+tomcat_release=`cat /tmp/tc${tomcat_major_version}-versions.txt | cut -d. -f3 | sort -nr | head -n1`
+tomcat_version=${tomcat_major_version}.${tomcat_minor_version}.${tomcat_release}
+tomcat_url=${mirrorbase}/tomcat-${tomcat_major_version}/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.tar.gz
 
 # Tidy up
-rm -f /tmp/tc${TOMCAT_MAJOR_VERSION}-index.html
-rm -f /tmp/tc${TOMCAT_MAJOR_VERSION}-versions.txt
+rm -f /tmp/tc${tomcat_major_version}-index.html
+rm -f /tmp/tc${tomcat_major_version}-versions.txt
 
 # Figure out where to download to based on the location of this script
-OUR_PATH=$(dirname $(readlink -f $0))
-DOWNLOAD_PATH=${OUR_PATH}/SOURCES/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+our_path=$(dirname $(readlink -f $0))
+download_path=${our_path}/SOURCES/apache-tomcat-${tomcat_version}.tar.gz
 
 # Debug output
-echo -e "Detected latest version: ${COLOR_SUCCESS}${TOMCAT_VERSION}${COLOR_RESET}"
-echo -e "Using URL: ${COLOR_SUCCESS}${TOMCAT_URL}${COLOR_RESET}"
-echo -e "Saving to: ${COLOR_SUCCESS}${DOWNLOAD_PATH}${COLOR_RESET}"
+echo -e "Detected latest version: ${color_success}${tomcat_version}${color_reset}"
+echo -e "Using URL: ${color_success}${tomcat_url}${color_reset}"
+echo -e "Saving to: ${color_success}${download_path}${color_reset}"
 echo
 echo "Downloading..."
 
 # Download Tomcat
-wget --quiet -O ${DOWNLOAD_PATH} $TOMCAT_URL
+wget --quiet -O ${download_path} $tomcat_url
 if [ "x$?" != "x0" ]; then
-	echo -e "${COLOR_FAILURE}Failed to download Tomcat${COLOR_RESET}"
+	echo -e "${color_failure}Failed to download Tomcat${color_reset}"
 	exit 1;
 fi
 
-echo -e "${COLOR_SUCCESS}Download succeeded.${COLOR_RESET} You should verify that the correct package has been downloaded."
+echo -e "${color_success}Download succeeded.${color_reset} You should verify that the correct package has been downloaded."
 
-echo -ne "\n${COLOR_BOLD}Update the RPM spec file to the latest version [y/n]? ${COLOR_RESET}"
-read UPDATE
+echo -ne "\n${color_bold}Update the RPM spec file to the latest version [y/n]? ${color_reset}"
+read update
 
-if [ "x$UPDATE" == "xy" ] || [ "x$UPDATE" == "xY" ]; then
-	SPEC_PATH_1=${OUR_PATH}/SPECS/tomcat.spec
-	#SPEC_PATH_2=${OUR_PATH}/SPECS/tomcatnative.spec
+if [ "x$update" == "xy" ] || [ "x$update" == "xY" ]; then
+	spec_path_1=${our_path}/SPECS/tomcat.spec
+	#SPEC_PATH_2=${our_path}/SPECS/tomcatnative.spec
 
 	# Get the version currently in the spec file
-	OLD_SPEC_VERSION=$(grep -E "^\s*%define\s+tomcat_version\s+" ${SPEC_PATH_1} | sed -r 's/^\s*%define\s+tomcat_version\s+//;s/\s*$//')
+	old_spec_version=$(grep -E "^\s*%define\s+tomcat_version\s+" ${spec_path_1} | sed -r 's/^\s*%define\s+tomcat_version\s+//;s/\s*$//')
 
 	# Update the spec file tomcat_version line
-	sed -ri "s/^(\s*%define\s+tomcat_version\s+)[0-9\.]+\s*/\1${TOMCAT_VERSION}/" ${SPEC_PATH_1}
+	sed -ri "s/^(\s*%define\s+tomcat_version\s+)[0-9\.]+\s*/\1${tomcat_version}/" ${spec_path_1}
 	if [ "x$?" != "x0" ]; then
-		echo -e "${COLOR_FAILURE}RPM spec file update failed${COLOR_RESET}"
+		echo -e "${color_failure}RPM spec file update failed${color_reset}"
 		exit 1
 	fi
-	#sed -ri "s/^(\s*%define\s+tomcat_version\s+)[0-9\.]+\s*/\1${TOMCAT_VERSION}/" ${SPEC_PATH_2}
+	#sed -ri "s/^(\s*%define\s+tomcat_version\s+)[0-9\.]+\s*/\1${tomcat_version}/" ${SPEC_PATH_2}
 	#if [ "x$?" != "x0" ]; then
-	#	echo -e "${COLOR_FAILURE}RPM spec file update failed${COLOR_RESET}"
+	#	echo -e "${color_failure}RPM spec file update failed${color_reset}"
 	#	exit 1
 	#fi
 
 	# If the version in the spec file has changed we should reset the 
 	# release back to 1
-	if [ "x${OLD_SPEC_VERSION}" != "x${TOMCAT_VERSION}" ]; then
-		echo "Previous spec file tomcat_version was ${OLD_SPEC_VERSION}. Resetting tomcat_release to 1."
-		sed -ri "s/^(\s*%define\s+tomcat_release\s+)[^\s]+\s*/\11/" ${SPEC_PATH_1}
+	if [ "x${old_spec_version}" != "x${tomcat_version}" ]; then
+		echo "Previous spec file tomcat_version was ${old_spec_version}. Resetting tomcat_release to 1."
+		sed -ri "s/^(\s*%define\s+tomcat_release\s+)[^\s]+\s*/\11/" ${spec_path_1}
 		if [ "x$?" != "x0" ]; then
-			echo -e "${COLOR_FAILURE}RPM spec file update failed${COLOR_RESET}"
+			echo -e "${color_failure}RPM spec file update failed${color_reset}"
 			exit 1
 		fi
 		#sed -ri "s/^(\s*%define\s+tomcat_release\s+)[^\s]+\s*/\11/" ${SPEC_PATH_2}
 		#if [ "x$?" != "x0" ]; then
-		#	echo -e "${COLOR_FAILURE}RPM spec file update failed${COLOR_RESET}"
+		#	echo -e "${color_failure}RPM spec file update failed${color_reset}"
 		#	exit 1
 		#fi
 	fi
 
-	echo -ne "${COLOR_BOLD}Build updated RPMs [y/n]? ${COLOR_RESET}"
-	read BUILD
+	echo -ne "${color_bold}Build updated RPMs [y/n]? ${color_reset}"
+	read build
 
-	if [ "x$BUILD" == "xy" ] || [ "x$BUILD" == "xY" ]; then
-		${OUR_PATH}/build.sh
+	if [ "x$build" == "xy" ] || [ "x$build" == "xY" ]; then
+		${our_path}/build.sh
 	fi
 fi
