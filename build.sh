8e8b60ba0349677e5dbbfc6a0883e595a5252e02
Richard J <techpowerawaits@outlook.com>
2021-04-14 16:13:51 -0600

    Fix sh-checker errors
    This increases code quality and fixes a few regressions from previous
    commits.

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # Check that we're not running as root!
-if [ $(id -u) -eq 0 ]; then
+if [ "$(id -u)" -eq 0 ]; then
 	echo "Do NOT run this script as root. Switch to the makerpm user first."
-	exit 1;
+	exit 1
 fi
 
 # Default to signing, but allow us not to
-if [ $1 == "--no-sign" ]; then
+if [ "$1" = "--no-sign" ]; then
 	sign=""
 else
 	sign="--sign"
 fi
 
 # Figure out the fully-qualified path of this script
-script_path=$(dirname $(readlink -f $0))
+script_path="$(dirname "$(readlink -f "$0")")"
 
-# Call rpmbuild, defining _topdir to be the fully qualified path to the 
+# Call rpmbuild, defining _topdir to be the fully qualified path to the
 # directory containing this script (which has an rpm buildroot in it)
-/bin/rpmbuild $sign --define "_topdir $script_path" -bb $script_path/SPECS/tomcat.spec
+/bin/rpmbuild "$sign" --define "_topdir $script_path" -bb "$script_path"/SPECS/tomcat.spec
--- get-latest.sh
+++ get-latest.sh
@@ -16,41 +16,40 @@ color_failure="\e[1;91m"
 color_reset="\e[0m"
 
 # Check configuration parameters
-if [ -z $mirrorbase ]; then
+if [ -z "$mirrorbase" ]; then
 	echo -e "${color_failure}mirrorbase not set - edit script configuration before running${color_reset}"
-	exit 1;
+	exit 1
 fi
-if [ -z $tomcat_major_version ]; then
+if [ -z "$tomcat_major_version" ]; then
 	echo -e "${color_failure}tomcat_major_version not set - edit script configuration before running${color_reset}"
-	exit 1;
+	exit 1
 fi
-if [ -z $tomcat_minor_version ]; then
+if [ -z "$tomcat_minor_version" ]; then
 	echo -e "${color_failure}tomcat_minor_version not set - edit script configuration before running${color_reset}"
-	exit 1;
+	exit 1
 fi
 
 # Download directory listing
-wget --quiet -O /tmp/tc${tomcat_major_version}-index.html "${mirrorbase}/tomcat-${tomcat_major_version}" > /dev/null
-if [ $? -ne 0 ]; then
+if ! wget --quiet -O /tmp/tc"${tomcat_major_version}"-index.html "${mirrorbase}/tomcat-${tomcat_major_version}" >/dev/null; then
 	echo -e "${color_failure}Tomcat version check download failed${color_reset}"
-	exit 1;
+	exit 1
 fi
 
 # Get a list of version numbers that are available
-cat /tmp/tc${tomcat_major_version}-index.html | fgrep '[DIR]' | grep -E "v${tomcat_major_version}.${tomcat_minor_version}.[0-9]+/" | grep -Eo '<a[ \t]+href[ \t]*=[ \t]*"[^"]*"' | grep -Eo "v${tomcat_major_version}\.${tomcat_minor_version}\.[^\"]*" | sed 's|/$||' > /tmp/tc${tomcat_major_version}-versions.txt
+grep -F '[DIR]' /tmp/tc"${tomcat_major_version}"-index.html | grep -E "v${tomcat_major_version}.${tomcat_minor_version}.[0-9]+/" | grep -Eo '<a[ \t]+href[ \t]*=[ \t]*"[^"]*"' | grep -Eo "v${tomcat_major_version}\.${tomcat_minor_version}\.[^\"]*" | sed 's|/$||' >/tmp/tc"${tomcat_major_version}"-versions.txt
 
 # Sort the release numbers to get the latest release
-tomcat_patch_version=`cat /tmp/tc${tomcat_major_version}-versions.txt | cut -d. -f3 | sort -nr | head -n1`
-tomcat_version=${tomcat_major_version}.${tomcat_minor_version}.${tomcat_patch_version}
+tomcat_patch_version=$(cut -d. -f3 /tmp/tc"${tomcat_major_version}"-versions.txt | sort -nr | head -n1)
+tomcat_version="${tomcat_major_version}"."${tomcat_minor_version}"."${tomcat_patch_version}"
 tomcat_url=${mirrorbase}/tomcat-${tomcat_major_version}/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.tar.gz
 
 # Tidy up
-rm -f /tmp/tc${tomcat_major_version}-index.html
-rm -f /tmp/tc${tomcat_major_version}-versions.txt
+rm -f /tmp/tc"${tomcat_major_version}"-index.html
+rm -f /tmp/tc"${tomcat_major_version}"-versions.txt
 
 # Figure out where to download to based on the location of this script
-script_path=$(dirname $(readlink -f $0))
-download_path=${script_path}/SOURCES/apache-tomcat-${tomcat_version}.tar.gz
+script_path="$(dirname "$(readlink -f "$0")")"
+download_path="${script_path}"/SOURCES/apache-tomcat-${tomcat_version}.tar.gz
 
 # Debug output
 echo -e "Detected latest version: ${color_success}${tomcat_version}${color_reset}"
@@ -60,45 +59,42 @@ echo
 echo "Downloading..."
 
 # Download Tomcat
-wget --quiet -O ${download_path} $tomcat_url
-if [ $? -ne 0 ]; then
+if ! wget --quiet -O "${download_path}" "$tomcat_url"; then
 	echo -e "${color_failure}Failed to download Tomcat${color_reset}"
-	exit 1;
+	exit 1
 fi
 
 echo -e "${color_success}Download succeeded.${color_reset} You should verify that the correct package has been downloaded."
 
 echo -ne "\n${color_bold}Update the RPM spec file to the latest version [y/n]? ${color_reset}"
-read update_prompt
+read -r update_prompt
 
-if [ $update_prompt == "y" ] || [ $update_prompt == "Y" ]; then
-	tc_spec_path=${script_path}/SPECS/tomcat.spec
+if [ "$update_prompt" = "y" ] || [ "$update_prompt" = "Y" ]; then
+	tc_spec_path="${script_path}"/SPECS/tomcat.spec
 
 	# Get the version currently in the spec file
-	spec_tomcat_version=$(grep -E "^\s*%define\s+tomcat_version\s+" ${tc_spec_path} | sed -r 's/^\s*%define\s+tomcat_version\s+//;s/\s*$//')
+	spec_tomcat_version=$(grep -E "^\s*%define\s+tomcat_version\s+" "${tc_spec_path}" | sed -r 's/^\s*%define\s+tomcat_version\s+//;s/\s*$//')
 
 	# Update the spec file tomcat_version line
-	sed -ri "s/^(\s*%define\s+tomcat_version\s+)[0-9\.]+\s*/\1${tomcat_version}/" ${tc_spec_path}
-	if [ $? -ne 0 ]; then
+	if ! sed -ri "s/^(\s*%define\s+tomcat_version\s+)[0-9\.]+\s*/\1${tomcat_version}/" "${tc_spec_path}"; then
 		echo -e "${color_failure}RPM spec file update failed${color_reset}"
 		exit 1
 	fi
 
-	# If the version in the spec file has changed we should reset the 
+	# If the version in the spec file has changed we should reset the
 	# release back to 1
-	if [ $spec_tomcat_version != $tomcat_version ]; then
+	if [ "$spec_tomcat_version" != "$tomcat_version" ]; then
 		echo "Previous spec file tomcat_version was ${spec_tomcat_version}. Resetting tomcat_patch_version to 1."
-		sed -ri "s/^(\s*%define\s+tomcat_patch_version\s+)[^\s]+\s*/\11/" ${spec_path_1}
-		if [ $? -ne 0 ]; then
+		if ! sed -ri "s/^(\s*%define\s+tomcat_patch_version\s+)[^\s]+\s*/\11/" "${tc_spec_path}"; then
 			echo -e "${color_failure}RPM spec file update failed${color_reset}"
 			exit 1
 		fi
 	fi
 
 	echo -ne "${color_bold}Build updated RPMs [y/n]? ${color_reset}"
-	read build_prompt
+	read -r build_prompt
 
-	if [ $build_prompt == "y" ] || [ $build_prompt == "Y" ]; then
-		${script_path}/build.sh
+	if [ "$build_prompt" = "y" ] || [ "$build_prompt" = "Y" ]; then
+		"${script_path}"/build.sh
 	fi
 fi
